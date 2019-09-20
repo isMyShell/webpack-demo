8cdb80edb316bfb0692a88f4e516acf2147c779d
yiqi <wangyiqi@guazi.com>
2019-09-20 16:00:01 +0800

    feat: 加载字体

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,14 @@ module.exports = {
                 {
                     test: /\.(png|svg|jpg|gif)$/,
                     use: [
-                        'file-loader' 
-                    ] 
+                            'file-loader' 
+                        ] 
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
+                            'file-loader' 
+                        ] 
                 }
             ] 
     }
