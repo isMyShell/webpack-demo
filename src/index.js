ac5603c9f7ad32b73fb51263066df394925ecf08
yiqi <wangyiqi@guazi.com>
2019-09-20 15:57:20 +0800

    feat: 加载图片

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css'
+import Icon from './icon.png';
 
 function component() {
     var element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
     
+    // 将图像添加到我们现有的 div
+    var myIcon = new Image(); 
+    myIcon.src = Icon; 
+    element.appendChild(myIcon);
+
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,13 @@ module.exports = {
                             'style-loader',
                             'css-loader' 
                         ] 
-                } 
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        'file-loader' 
+                    ] 
+                }
             ] 
     }
 };
