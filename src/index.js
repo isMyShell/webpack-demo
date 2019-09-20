c8b2d96441363c5048b17d2e272c13f6e63127a1
yiqi <wangyiqi@guazi.com>
2019-09-20 16:02:08 +0800

    feat: 加载数据

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css'
 import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
@@ -11,8 +12,11 @@ function component() {
     // 将图像添加到我们现有的 div
     var myIcon = new Image(); 
     myIcon.src = Icon; 
+
     element.appendChild(myIcon);
 
+    console.log(Data);
+    
     return element;
 }
 
